1fa7dbf3bf871229a589111693af31bfd6b4ea75
Git1403 <f.taghipoor1403@gmail.com>
2026-09-07 12:26:02 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+ return {
+    
+  base: '/Www/',
+  plugins: [react(), tailwindcss()],,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
